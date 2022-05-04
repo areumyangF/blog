2219ac18bb644b9c8a8dff356f6a58a904cf7262
areumyangf <areum.yang@finiview.com>
2022-05-04 17:00:27 +0900

    Update: main meta tag

--- pages/main.vue
+++ pages/main.vue
@@ -16,6 +16,48 @@ export default {
     FirstWrap,
     SecondWrap,
     NavBar
+  },
+  head() {
+    return {
+      title: 'nuxt content 를 이용한 Blog 관리',
+      meta: [
+        {
+          hid: 'description', // nuxt 의 hid 라는 유니크한 식별자를 통해 meta 태그를 덮어 씌어준다.
+          name: 'description',
+          content:
+            'content 를 이용해 md 문서를 작성하고 localhost liveview 로 편집을 할 수 있다.'
+        },
+        {
+          hid: 'keywords',
+          name: 'keywords',
+          content: ['nuxtjs', 'javascript', 'mdx']
+        },
+        {
+          hid: 'og:title',
+          property: 'og:title',
+          content: 'nuxt content 를 이용한 Blog 관리'
+        },
+        {
+          hid: 'og:description',
+          property: 'og:description',
+          content:
+            'content 를 이용해 md 문서를 작성하고 localhost liveview 로 편집을 할 수 있다.'
+        },
+        {
+          hid: 'og:image',
+          property: 'og:image',
+          // content: 'http://placeimg.com/640/480/fashion'
+          content: '/images/bg-studio.png'
+        }
+      ],
+      link: [
+        {
+          hid: 'canonical',
+          rel: 'canonical',
+          href: this.meta.url
+        }
+      ]
+    }
   }
 }
 </script>
